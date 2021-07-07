10846576b703973e803e2e9f66daa7d4a6e13f84
vovkahimik <vovkahimik@gmail.com>
2021-07-07 18:24:57 +0300

    binary_search

---
+++ binary_search.js
@@ -0,0 +1,21 @@
+function binary_search(list, item) {
+    let low = 0
+    let high = list.length - 1
+    while (low <= high) {
+        let tempMid = (low + high) / 2
+        let mid = tempMid % 2 ? Math.floor(tempMid) : tempMid
+        let guess = list[mid]
+        if (guess === item) {
+            return mid
+        }
+        if (guess > item) {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
+}
+
+console.log(
+    binary_search([1, 3, 5, 7, 9, 10], 1)
+)
